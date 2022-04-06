1546ff9069cc3494dd6792e42df7845a0585f76d
Ixabel Justo <71872946+Kay-Nicte@users.noreply.github.com>
2022-04-06 13:32:20 +0200

    "Ejercicio 1"

--- UD05/src/UD05/Ejercicio1.java
+++ UD05/src/UD05/Ejercicio1.java
@@ -9,6 +9,40 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//Declaro dos variables numéricas y las inicializo
+		
+		int primeraVariable = 5;
+		int segundaVariable = 36;
+		
+		//Declaro una variable max que me dirá qué variable tiene el valor mayor
+		
+		int max = Math.max(primeraVariable, segundaVariable);
+		//Busco el valor mayor de los dos y lo imprimo por pantalla (en caso de que no sean iguales)
+		
+		System.out.print("El valor mayor de los dos es: "+max+"\n");
+		
+		//Cambio el valor de la primera variable para que ésta sea la mayor
+		
+		primeraVariable = 70;
+		max = Math.max(primeraVariable, segundaVariable);
+		
+		System.out.print("El valor mayor de los dos es: "+max+"\n");
+
+		//Cambio el valor de las variables para que sean iguales 
+		
+		primeraVariable = 5;
+		segundaVariable = 5;
+		max = Math.max(primeraVariable, segundaVariable);
+		
+		/*Voy a comprobar con un if si las dos variables son diferentes
+		 * Como no lo son, pasará directamente al else.
+		 */
+		
+		if (primeraVariable != segundaVariable) {
+			System.out.print("El valor mayor de los dos es: "+max);
+			}else {
+				System.out.println("Las dos variables tienen el mismo valor: "+primeraVariable);
+			}
 	}
 
 }
