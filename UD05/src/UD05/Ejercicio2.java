43828308bffcc3fe54ef3d6619c56ff831b51307
Ixabel Justo <71872946+Kay-Nicte@users.noreply.github.com>
2022-04-06 13:35:19 +0200

    "Ejercicio 2"

---
+++ UD05/src/UD05/Ejercicio2.java
@@ -0,0 +1,18 @@
+package UD05;
+
+public class Ejercicio2 {
+
+	public Ejercicio2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Declaro un String que contenga mi nombre
+		
+		String nombre = "Ixa";
+		System.out.println("¡Bienvenid@ "+nombre+"!");
+	}
+
+}
