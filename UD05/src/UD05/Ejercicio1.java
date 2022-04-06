121b0878b95cb6d2053d578e66461eee6f5017b7
Ixabel Justo <71872946+Kay-Nicte@users.noreply.github.com>
2022-04-06 13:11:36 +0200

    "UD05"

---
+++ UD05/src/UD05/Ejercicio1.java
@@ -0,0 +1,14 @@
+package UD05;
+
+public class Ejercicio1 {
+
+	public Ejercicio1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
