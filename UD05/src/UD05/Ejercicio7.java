2c22d5cb2b830679374a62fadb710331ed08ebbc
Ixabel Justo <71872946+Kay-Nicte@users.noreply.github.com>
2022-04-06 14:38:17 +0200

    "Ejercicio 7"

---
+++ UD05/src/UD05/Ejercicio7.java
@@ -0,0 +1,27 @@
+package UD05;
+
+public class Ejercicio7 {
+
+	public Ejercicio7() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*Aplicación para mostrar todos los números del 1 al 100, ambos incluidos
+		 * Utilizar un bucle while
+		 */
+		
+		//Declaro e inicializo la variable de contador
+		int i = 1;
+		
+		//Abro el bucle con la condición de que mientras el contador sea menor o igual que 100, imprima y se siga incrementando
+		while(i <= 100) {
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+
+}
