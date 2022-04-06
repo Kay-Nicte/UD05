581713a22aca430c3bc3f459b88fc515b354312b
Ixabel Justo <71872946+Kay-Nicte@users.noreply.github.com>
2022-04-06 14:45:11 +0200

    "Ejercicio 8"

---
+++ UD05/src/UD05/Ejercicio8.java
@@ -0,0 +1,22 @@
+package UD05;
+
+public class Ejercicio8 {
+
+	public Ejercicio8() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*Aplicación para mostrar todos los números del 1 al 100, ambos incluidos
+		 * Utilizar un bucle for
+		 */
+		
+			
+		//Abro el bucle con la condición de que mientras el contador sea menor o igual que 100, imprima
+		for(int i=1; i<= 100; i++) {
+			System.out.println(i);
+		}
+	}
+}
